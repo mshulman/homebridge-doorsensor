b715e98c34513a77e521f89c6f80532be47d2c0a
pdemarco925 <pdemarco25@comcast.net>
2018-02-12 21:19:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,117 @@
 //        {
 //             "accessory": "DoorSensor",
 //             "name": "Garage Door",
-//	       "sensorPin": 18,
-//	       "sensorPollInMs": 5000
+//             "sensorPollInMs": 5000,
+//             "flipBehavior": false,
+//             "sensorPin": 18,         /* sensorPin or triggerFile required */
+//             "triggerFile": "file"    /* but not both */
 //        }
 
 var Service, Characteristic, DoorState;
 var rpio = require('rpio');
+var fs = require('fs');
 
 module.exports = function(homebridge){
-	Service = homebridge.hap.Service;
-	Characteristic = homebridge.hap.Characteristic;
-	DoorState = homebridge.hap.Characteristic.CurrentDoorState;
-	homebridge.registerAccessory("homebridge-doorsensor", "DoorSensor", DoorSensorAccessory);
+        Service = homebridge.hap.Service;
+        Characteristic = homebridge.hap.Characteristic;
+        DoorState = homebridge.hap.Characteristic.CurrentDoorState;
+        homebridge.registerAccessory("homebridge-doorsensor", "DoorSensor", DoorSensorAccessory);
 };
 
 function DoorSensorAccessory(log, config) {
-	this.log = log;
-	this.name = config.name;
-	this.sensorPin = config.sensorPin;
-	rpio.open(this.sensorPin,rpio.INPUT, rpio.PULL_DOWN);
+        this.log = log;
+        this.name = config.name;
+        this.sensorPollInMs = config.sensorPollInMs;
+        this.flipBehavior = config.flipBehavior;
 
-	this.sensorPollInMs = config.sensorPollInMs;
+        this.triggerFile = config.triggerFile || null;
+        this.sensorPin = config.sensorPin || null;
 
-	this.isClosed = true; 
-	this.wasClosed = true; 
+        if (this.sensorPin == null && this.triggerFile == null){
+          this.log("Need to define a sensorPin or a triggerFile!");
+          process.exit(1);
+        }
 
-	this.service = new Service.ContactSensor(this.name);
-	setTimeout(this.monitorDoorState.bind(this), this.sensorPollInMs);
+        if (this.sensorPin != null){
+          rpio.open(this.sensorPin,rpio.INPUT, rpio.PULL_DOWN);
+        }
+
+        if (this.triggerFile != null){
+          if (!fs.existsSync(this.triggerFile)){
+            this.log("File '" + this.triggerFile + "' not found!");
+            process.exit(1);
+          }
+        }
+
+        this.isClosed = true;
+        this.wasClosed = true;
+
+        this.service = new Service.ContactSensor(this.name);
+        setTimeout(this.monitorDoorState.bind(this), this.sensorPollInMs);
 };
 
 DoorSensorAccessory.prototype = {
-	identify: function(callback) {
-		this.log("Identify requested!");
-		callback(null);
-	},
+        identify: function(callback) {
+                this.log("Identify requested!");
+                callback(null);
+        },
 
-	monitorDoorState: function() {
-		this.isClosed = this.isDoorClosed();
-		if (this.isClosed != this.wasClosed) {
-			this.wasClosed = this.isClosed;
-			this.service.getCharacteristic(Characteristic.ContactSensorState).setValue(this.isClosed);
-	                this.log("monitorDoorState: " + this.isClosed);
-		}
-		setTimeout(this.monitorDoorState.bind(this), this.sensorPollInMs);
-	},
+        monitorDoorState: function() {
+                this.isClosed = this.isDoorClosed();
+                if (this.isClosed != this.wasClosed) {
+                        this.wasClosed = this.isClosed;
+                        this.service.getCharacteristic(Characteristic.ContactSensorState).setValue(this.isClosed);
+                        this.log("monitorDoorState: " + this.isClosed);
+                }
+                setTimeout(this.monitorDoorState.bind(this), this.sensorPollInMs);
+        },
 
-	isDoorClosed: function() {
-		return !rpio.read(this.sensorPin);
-	},
+        isDoorClosed: function() {
+                if (this.triggerFile != null){
+                        if (this.flipBehavior) {
+                          return parseInt(fs.readFileSync(this.triggerFile, 'utf8'));
+                        } else {
+                          return parseInt(fs.readFileSync(this.triggerFile, 'utf8'));
+                        }
+                }
 
-	getContactSensorState: function(callback) {
-		this.isClosed = this.isDoorClosed();
-		this.log("getContactSensorState: ", this.isClosed);
-		callback(null, this.isClosed);
-	},
 
-	getName: function(callback) {
-		this.log("getName :", this.name);
-		callback(null, this.name);
-	},
+                if (this.sensorPin != null){
+                        if (this.flipBehavior) {
+                          return rpio.read(this.sensorPin);
+                        } else {
+                          return !rpio.read(this.sensorPin);
+                        }
+                }
+        },
 
-	getServices: function() {
-		var informationService = new Service.AccessoryInformation();
+        getContactSensorState: function(callback) {
+                this.isClosed = this.isDoorClosed();
+                this.log("getContactSensorState: ", this.isClosed);
+                callback(null, this.isClosed);
+        },
 
-		informationService
-			.setCharacteristic(Characteristic.Manufacturer, "DoorSensor")
-			.setCharacteristic(Characteristic.Model, "MyDoor")
-			.setCharacteristic(Characteristic.SerialNumber, "Version 0.0.1");
+        getName: function(callback) {
+                this.log("getName :", this.name);
+                callback(null, this.name);
+        },
 
-		this.service
-			.getCharacteristic(Characteristic.ContactSensorState)
-			.on('get', this.getContactSensorState.bind(this));
+        getServices: function() {
+                var informationService = new Service.AccessoryInformation();
 
-		this.service
-			.getCharacteristic(Characteristic.Name)
-			.on('get', this.getName.bind(this));
+                informationService
+                        .setCharacteristic(Characteristic.Manufacturer, "DoorSensor")
+                        .setCharacteristic(Characteristic.Model, "MyDoor")
+                        .setCharacteristic(Characteristic.SerialNumber, "Version 0.0.1");
 
-		return [informationService, this.service];
-	}
+                this.service
+                        .getCharacteristic(Characteristic.ContactSensorState)
+                        .on('get', this.getContactSensorState.bind(this));
+
+                this.service
+                        .getCharacteristic(Characteristic.Name)
+                        .on('get', this.getName.bind(this));
+
+                return [informationService, this.service];
+        }
 };
